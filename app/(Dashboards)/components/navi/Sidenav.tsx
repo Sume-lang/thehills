13fde8fe08526f380a04d72304eb7190a091d167
Advsum <sumekah33@gmail.com>
2024-09-15 00:57:26 +0800

    adding the my

--- app/(Dashboards)/components/navi/Sidenav.tsx
+++ app/(Dashboards)/components/navi/Sidenav.tsx
@@ -1,28 +1,19 @@
 "use client";
-import Link from "next/link";
-import { Layers } from "lucide-react";
 const Navitems = [
   {
     title: "Web Management",
-    items: [
-      {
-        name: "Pages",
-        url: "/",
-        icons: <Layers className="w-5 h-5" />,
-        child: [
-          {
-            name: "Create Post",
-            url: "/",
-            icons: "",
-          },
-          {
-            name: "Edit Post",
-            url: "/",
-            icons: "",
-          },
-        ],
-      },
-    ],
+  },
+  {
+    title: "Revenues",
+  },
+  {
+    title: "Booking",
+  },
+  {
+    title: "Data Service",
+  },
+  {
+    title: "Settings",
   },
 ];
 
@@ -37,19 +28,9 @@ function Sidebar() {
       <section>
         {Navitems.map((i) => (
           <div key={i.title} className="flex flex-col">
-            <span className="hidden lg:block text-slate-900 font-bold my-4">
+            <span className="hidden lg:block text-slate-900 font-bold my-10">
               {i.title}
             </span>
-            {i.items.map((items) => (
-              <Link
-                key={items.name}
-                href=""
-                className="flex items-center justify-center lg:justify-start gap-4 p-2 hover:bg-gray-500 hover:text-slate-100 hover:shadow-md hover:font-bold text-slate-900 duration-300 ease-out rounded-md cursor-pointer"
-              >
-                <span className="">{items.icons}</span>
-                <span className="hidden lg:block">{items.name}</span>
-              </Link>
-            ))}
           </div>
         ))}
       </section>
