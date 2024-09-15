c0154acbeff98236054efba89ae63bdcb7a95e52
Advsum <sumekah33@gmail.com>
2024-09-16 07:11:28 +0800

    adding the function button

--- app/section/Activities.tsx
+++ app/section/Activities.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Img from "next/image";
+import Datatour from "./data/Datatour";
 
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
@@ -62,12 +63,31 @@ export const Rinjani: Activites[] = [
     image: "/Assets/Images/Beach-4.jpg",
   },
 ];
-
 export default function Activites() {
   return (
-    <main className="w-full min-h-screen flex lg:flex-row flex-col items-start justify-start gap-4 lg:p-20 p-2">
-      <section>
-        <ScrollArea className="lg:w-[800px] w-[350px] whitespace-nowrap rounded-md border shadow-md">
+    <main className="w-full min-h-screen flex lg:flex-col flex-col items-start justify-start gap-4 lg:p-20 p-2">
+      <section className="w-1/2 h-full">
+        <h1 className="text-5xl font-extrabold uppercase">Activities</h1>
+        <h1 className="font-extralight">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          vero fugit recusandae, sunt neque in explicabo. Placeat porro
+          consequuntur rem labore autem facere nam ullam minus ab, cupiditate
+          quam reprehenderit.
+        </h1>
+        <div className="flex flex-row h-[100%] gap-2 py-4">
+          {Datatour.map((i) => (
+            <div
+              key={i.name}
+              className="flex flex-col p-2 border rounded-md w-1/6 items-start"
+            >
+              <h1 className="text-xl font-bold">{i.qty}+</h1>
+              <span>{i.name}</span>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="w-full h-full">
+        <ScrollArea className="lg:w-full w-[350px] whitespace-nowrap rounded-md border shadow-md">
           <div className="flex w-max space-x-4 lg:p-4">
             {Rinjani.map((Activites) => (
               <figure key={Activites.name} className="shrink-0">
@@ -91,22 +111,6 @@ export default function Activites() {
           </div>
           <ScrollBar orientation="horizontal" />
         </ScrollArea>
-      </section>
-      <section className="w-full h-full">
-        <h1 className="text-5xl font-extrabold uppercase">Activities</h1>
-        <h1 className="font-extralight">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          vero fugit recusandae, sunt neque in explicabo. Placeat porro
-          consequuntur rem labore autem facere nam ullam minus ab, cupiditate
-          quam reprehenderit.
-        </h1>
-        <div className="flex flex-row p-2 h-[100%] gap-2">
-          <div className="flex flex-col p-2 shadow-md">
-            <h1 className="text-xl font-extralight uppercase">
-              Beaches Activities
-            </h1>
-          </div>
-        </div>
       </section>
     </main>
   );
---
+++ app/section/data/Datatour.js
@@ -0,0 +1,23 @@
+const DataTour = [
+    {
+        name: "Beach",
+        qty:"50",
+    },
+    {
+        name: "Hills and Mountain",
+        qty: "20",
+    },
+    {
+        name: "Waterfalls",
+        qty: "20",
+    },
+    {
+        name: "Cultures",
+        qty: "10",
+    },
+    {
+        name: "Hotels",
+        qty:"81"
+    }
+]
+export default DataTour
