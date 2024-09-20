128fd190b34217415e01a037867313a8548dff3c
Advsum <sumekah33@gmail.com>
2024-09-21 00:38:15 +0800

    adding the files

---
+++ app/(pages)/section/datatest.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import FlipCard from "../../../components/ui/Flipcard";
+
+export default function Datatest() {
+  return (
+    <div>
+      <FlipCard>
+        <h1>hello</h1>
+      </FlipCard>
+    </div>
+  );
+}
