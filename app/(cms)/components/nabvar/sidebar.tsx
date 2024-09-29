40fe093af805e1d52cc98e1882aabbd122975b8e
Advsum <sumekah33@gmail.com>
2024-09-29 20:38:55 +0800

    modified

--- app/(cms)/components/nabvar/sidebar.tsx
+++ app/(cms)/components/nabvar/sidebar.tsx
@@ -17,6 +17,36 @@ const menuItems = [
     icons: <Home size={48} strokeWidth={1.5} />,
     href: "/",
   },
+  {
+    id: 2,
+    name: "About",
+    icons: <Home size={48} strokeWidth={1.5} />,
+    href: "/",
+  },
+  {
+    id: 3,
+    name: "Services",
+    icons: <Home size={48} strokeWidth={1.5} />,
+    href: "/",
+  },
+  {
+    id: 4,
+    name: "Packages",
+    icons: <Home size={48} strokeWidth={1.5} />,
+    href: "/",
+  },
+  {
+    id: 5,
+    name: "Activities",
+    icons: <Home size={48} strokeWidth={1.5} />,
+    href: "/",
+  },
+  {
+    id: 6,
+    name: "Destination",
+    icons: <Home size={48} strokeWidth={1.5} />,
+    href: "/",
+  },
 ];
 const settings = [
   {
@@ -47,21 +77,39 @@ const settings = [
 
 export default function sideNavigations() {
   return (
-    <main className="w-full min-h-screen py-6 p-4 flex flex-col text-white">
-      <section id="logo">Logo</section>
-      <section id="menuItems">Menu Items</section>
+    <main className="sticky top-0 left-0 w-full min-h-screen py-6 p-4 flex flex-col justify-between text-white">
+      <section id="menuItems">
+        <div id="logo">
+          {Logo.map((item) => (
+            <span
+              key={item.id}
+              className="flex flex-col items-start justify-start gap-2">
+              <img src={item.img} alt="logo" className="w-10 h-10" />
+            </span>
+          ))}
+        </div>
+        <div className="flex flex-col items-start justify-start py-4">
+          <h1 className="text-3xl font-bold text-slate-50">Home</h1>
+          {menuItems.map((item) => (
+            <div
+              key={item.id}
+              className="flex flex-col items-start justify-start gap-2">
+              <h1 className="text-2xl font-light text-slate-50">{item.name}</h1>
+            </div>
+          ))}
+        </div>
+      </section>
       <section
         id="settings"
         className="flex flex-col items-start justify-start">
-        <div className="flex flex-row items-center justify-start gap-2 rounded-md">
-          <h1 className="text-md font-bold text-slate-600">Settings</h1>
+        <div className="flex flex-row items-center justify-start gap-2">
+          <h1 className="text-3xl font-bold text-slate-50">Settings</h1>
         </div>
-
         {settings.map((item) => (
           <div
             key={item.id}
-            className="flex flex-col items-start justify-start gap-4">
-            <h1 className="text-md font-light text-slate-800">{item.name}</h1>
+            className="flex flex-col items-start justify-start gap-2">
+            <h1 className="text-2xl font-light text-slate-50">{item.name}</h1>
           </div>
         ))}
       </section>
