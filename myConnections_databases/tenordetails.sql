2a8646cc17a97ea6e4c749cef27c5647ae5a6284
Advsum <sumekah33@gmail.com>
2024-10-04 13:01:43 +0800

    adding and modified

--- myConnections_databases/tenordetails.sql
+++ myConnections_databases/tenordetails.sql
@@ -1,4 +1,4 @@
-USE DATABASE thehills;
+USE thehills;
 
 
 -- drivers tables
--- myConnections_databases/user.sql
+++ myConnections_databases/user.sql
@@ -1,1 +1,13 @@
-USE DATABASE thehills;
+USE thehills;
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
+    roles ENUM ('administrator','financial','drivers','guides','author') NOT NULL,
+    gender ENUM ('Male', 'Female') NOT NULL,
+    addresses VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
